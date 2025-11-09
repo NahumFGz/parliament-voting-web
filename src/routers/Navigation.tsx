ae8fffe4c1f13bb4ebef3321fd096243ac6d9762
NahumFGz <ngutierrez2507@gmail.com>
2025-11-09 05:38:42 -0500

    feat: :sparkles: Agregando ruta base

--- src/routers/Navigation.tsx
+++ src/routers/Navigation.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomeLayout } from '../features/home/layouts/HomeLayout'
 import { HomePage } from '../features/home/pages/HomePage'
 import { ErrorLayout } from '../features/error/layouts/ErrorLayout'
@@ -9,8 +9,7 @@ export function Navigation() {
     <BrowserRouter>
       <Routes>
         <Route element={<HomeLayout />}>
-          <Route index element={<Navigate to='/home' replace />} />
-          <Route path='/home' element={<HomePage />} />
+          <Route path='/' element={<HomePage />} />
         </Route>
 
         {/* Rutas para error */}
